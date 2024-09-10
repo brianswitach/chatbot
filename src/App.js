01863e463078aabcb94c3b2311cedf5db93e133b
Brian Switach <brianswitach@Air-de-Brian.fibertel.com.ar>
2024-09-10 14:56:31 -0300

    changes'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from './components/Footer';
 import './App.css';
 import Features from './components/Features';
 import DemoSection from './components/DemoSection';
-import Testimonials from './components/Testimonials';
 import Bot from './components/Bot'; // Importa el componente Bot
 
 function App() {
@@ -18,8 +17,6 @@ function App() {
       <DemoSection />
       <div className="divider2"></div> {/* Línea divisoria negra */}
       <Bot />
-      <div className="divider2"></div> {/* Línea divisoria negra */}
-      <Testimonials /> {/* Agrega la sección de testimonios */}
       <div className="divider2"></div> {/* Línea divisoria negra */}
       <Footer />
     </div>
